4deabb471dbcc6560dcfa17f74003caeed714628
Brian Coyle <bobo.coyle@gmail.com>
2015-03-23 23:57:50 -0400

    Revised app.get to check database instead of using static text

--- server.js
+++ server.js
@@ -4,13 +4,11 @@ var bodyParser = require('body-parser')
 var app = express()
 app.use(bodyParser.json())
 
-app.get('/api/posts', function (req, res) {
-    res.json([
-        {
-            username: 'dickeyxxx',
-            body: 'node rocks!'
-        }
-    ])
+app.get('/api/posts', function (req, res, next) {
+    Post.find(function(err,posts) {
+        if (err) {return next(err)}
+        res.json(posts)
+    })
 });
 
 var Post = require('./models/post')
