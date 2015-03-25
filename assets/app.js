164c519a0028e5df1d2d857075d0075e6e4b6388
Brian Coyle <bobo.coyle@gmail.com>
2015-03-24 20:21:32 -0400

    Served static assets from app.js.  Moved angular outside of HTML.  Broke out sendfile endpoint in to static.js

---
+++ assets/app.js
@@ -0,0 +1,19 @@
+var app = angular.module('app', [])
+app.controller('PostsCtrl', function ($scope, $http) {
+
+    $scope.addPost = function () {
+        if ($scope.postBody) {
+            $http.post('/api/posts', {
+                username: 'Brian',
+                body: $scope.postBody
+            }).success(function (post) {
+                $scope.posts.unshift(post)
+                $scope.postBody = null
+            })
+        }
+    }
+    $http.get('/api/posts').success(function (posts) {
+        $scope.posts = posts
+    })
+
+})
--- server.js
+++ server.js
@@ -4,20 +4,7 @@ var bodyParser = require('body-parser')
 var app = express()
 app.use(bodyParser.json())
 app.use('/api/posts',require('./controllers/api/posts'))
-
-
-app.get('/', function (req, res) {
-    res.sendfile('layouts/posts.html', function (err){
-        if (err) {
-            console.log(err);
-            res.status(err.status).end();
-        }
-        
-        else {
-            console.log('Sent:', 'layouts/posts.html');
-        }
-    });
-})
+app.use(require('./controllers/static'))
 
 app.listen(3000, function () {
     console.log('Server listening on', 3000)
